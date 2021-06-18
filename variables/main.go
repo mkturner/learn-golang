297e30a45f9678eb715fd776fe5e96266968bbac
Marvin T <contact@marvinkturner.com>
2021-06-17 23:57:53 -0400

    practice initial value, type inference, multiple variables
    Signed-off-by: Marvin T <contact@marvinkturner.com>

--- variables/main.go
+++ variables/main.go
@@ -18,4 +18,25 @@ func main() {
 	// Multiple variable declaration
 	var fname, lname = "Marvin", "Turner"
 	fmt.Println("My full name is:", fname, lname)
+
+	// declaring with initial value
+	var brosAge int = 8
+	fmt.Println("My brother's age is", brosAge)
+
+	// could also be written as:
+	// var age = 8
+	// when initial value present, type may be removed
+
+	// multiple value declaration
+	var width, height int = 100, 50
+	fmt.Println("width is:", width, "height is:", height, "area is:", (width * height))
+
+	// declaring variables of different types
+	var (
+		myName   = "marv"
+		myAge    = 29
+		myHeight int
+	)
+
+	fmt.Println("my name is", myName, "age is", myAge, "and height is", myHeight)
 }
